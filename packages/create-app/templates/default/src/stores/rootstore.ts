cf0bebe18227c9be16c3358e359d3519e012284e
Jean-François Pustay <jef97310@gmail.com>
2021-05-15 15:24:37 +0200

    let empty youhou

--- packages/create-app/templates/default/src/stores/rootstore.ts
+++
@@ -1,9 +0,0 @@
-import { createStoreContext } from "../core/hooks";
-import { UIStore } from "./uistore";
-
-export class RootStore {
-    ui = new UIStore();
-}
-
-const [useRoot, RootProvider] = createStoreContext<RootStore>();
-export { useRoot, RootProvider };
